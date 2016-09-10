6f102714da20d1b677e46f2a4ef539161b74d5d8
kuladeep93 <s.kuladeep93@gmail.com>
2016-09-10 12:32:51 +0530

    carousel bug resolved

--- js/stylesheet.js
+++ js/stylesheet.js
@@ -7,29 +7,9 @@ $(document).ready(function () {
     wow = new WOW(
             {
                 animateClass: 'animated',
-                offset: 300
+                offset: 150
             }
     );
     wow.init();
 
-});
-var $item = $('.carousel .item');
-var $wHeight = $(window).height();
-$item.eq(0).addClass('active');
-$item.height($wHeight);
-$item.addClass('full-screen');
-
-$('.carousel img').each(function () {
-    var $src = $(this).attr('src');
-    var $color = $(this).attr('data-color');
-    $(this).parent().css({
-        'background-image': 'url(' + $src + ')',
-        'background-color': $color
-    });
-    $(this).remove();
-});
-
-$(window).on('resize', function () {
-    $wHeight = $(window).height();
-    $item.height($wHeight);
 });
